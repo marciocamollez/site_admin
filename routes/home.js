834901d9f0631cfa90055fac9eb580b0d11ad88a
Marcio <marciocamollez@hotmail.com>
2020-11-10 14:02:43 -0300

    Acertos da pasta node_modules

--- routes/home.js
+++ routes/home.js
@@ -1,14 +1,19 @@
 //Carregando os módulos
 const express = require('express')
-const mongoose = require('mongoose')
 const router = express.Router()
+const mongoose = require('mongoose')
+
 require("../models/HomeTopo")
 const HomeTopo = mongoose.model('hometopos')
 
 router.get('/', (req,res) => {
     HomeTopo.findOne({}).then((hometopo) => {
         console.log(hometopo)
-        res.render("home/home", { hometopo: hometopo }) //pasta home, arquivo home.handlebars
+        res.render("home/home", { hometopo: hometopo }) 
+        //pasta home, arquivo home.handlebars
+        // primeiro parametro é o nome que vai ficar dentro da chave no html e o segundo
+        // é o nome do modelo. Exemplo { nome_do_html: nome_do_modelo }
+
     }).catch((erro) =>{
         res.send("Nenhum topo encontrado")
     })
