1c19c9fd3a155959313f71693803bff01d092dba
Marcio <marciocamollez@hotmail.com>
2020-11-12 15:04:26 -0300

    Validacao do Form de contato

--- routes/contato.js
+++ routes/contato.js
@@ -26,21 +26,48 @@ router.get('/', (req,res) => {
 })
 
 
+/* Pega os dados digitados do form, verifica se não está em branco. Caso esteja algum faltando, apresenta mensagem de erro e recarrega a página. Caso dê certo, avança para salvar no banco de dados */
 router.post('/add-contato', (req, res) => {
-     const addContato = {
-         nome: req.body.nome,
-         email: req.body.email,
-         assunto: req.body.assunto,
-         mensagem: req.body.mensagem
-     }
+    var dados_contato = req.body
+    var errors = []
+    if (!req.body.nome || typeof req.body.nome == undefined || req.body.nome == null) {
+        errors.push({ error: "Necessário preencher o campo nome!" })
+    }
+    if (!req.body.email || typeof req.body.email == undefined || req.body.email == null) {
+        errors.push({ error: "Necessário preencher o campo e-mail!" })
+    }
+    if (!req.body.assunto || typeof req.body.assunto == undefined || req.body.assunto == null) {
+        errors.push({ error: "Necessário preencher o campo assunto!" })
+    }
+    if (!req.body.mensagem || typeof req.body.mensagem == undefined || req.body.mensagem == null) {
+        errors.push({ error: "Necessário preencher o campo mensagem!" })
+    }
 
-     new Contato(addContato).save().then(() => {
-        console.log('Cadastrado com sucesso')
-        res.redirect('/contato')
-     }).catch((erro) => {
-        console.log('Falha ao cadastrar')
-        res.redirect('/contato')
-     })
+    if (errors.length > 0) {
+        ContatoInfo.findOne({}).then((contatoinfo) => {
+            Rodape.findOne({}).then((rodape) => {
+                res.render("contato/contato", { errors: errors, contato: dados_contato, contatoinfo: contatoinfo, rodape: rodape })
+            }).catch((erro) => {
+                res.send("Nenhuma informação encontrada entre em contato com o administrador!")
+            })
+        }).catch((erro) => {
+            res.send("Nenhuma informação encontrada entre em contato com o administrador!")
+        })
+    } else {
+        const addContato = {
+            nome: req.body.nome,
+            email: req.body.email,
+            assunto: req.body.assunto,
+            mensagem: req.body.mensagem
+        }
+        new Contato(addContato).save().then(() => {
+            console.log("Contato cadastrado com sucesso")
+            res.redirect('/contato')
+        }).catch((erro) => {
+            console.log("Erro: Contato não foi cadastrado com sucesso")
+            res.redirect('/contato')
+        })
+    }
 })
 
 
