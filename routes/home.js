b3656cb78ffa0b015a484502cc16579030f4aadf
Marcio <marciocamollez@hotmail.com>
2020-11-23 11:26:02 -0300

    Insercao de Form editar Servico

--- routes/home.js
+++ routes/home.js
@@ -162,4 +162,100 @@ router.post('/update-home-top-img', upload.single('file'), (req, res, next) => {
     }
 })
 
+router.get('/vis-home-serv', eAdmin, (req, res) => {
+    Servico.findOne({}).then((servico) => {
+        res.render("home/vis-home-serv", {layout: "adm.handlebars", servico: servico})
+    }).catch((erro) =>{
+        req.flash("error_msg", "Erro: Não foi possível encontrar o Serviço")
+        res.redirect("/dashboard/")
+    })
+})
+
+router.get('/edit-home-serv', eAdmin, (req,res) => {
+    Servico.findOne({}).then((servico) => {
+        res.render("home/edit-home-serv", { layout: 'adm.handlebars', servico: servico }) 
+    }).catch((erro) => {
+        req.flash("error_msg", "Erro: Não foi possível encontrar o Serviço")
+        res.redirect("/dashboard/")
+    })
+    
+})
+
+router.post('/update-home-serv', eAdmin, (req,res) => {
+
+    //Primeiro pega todos os campos e verifica se estão vazios, nulos ou indefinidos
+    //Caso esteja, apresenta mensagem de erro em cima do formulário
+    var dados_home_serv = req.body
+    var errors = []
+
+    if(!req.body.titulo || typeof req.body.titulo == undefined || req.body.titulo == null){
+        errors.push({error: "Necessário preencher o campo título do Serviço"})
+    }
+    if(!req.body.iconeservum || typeof req.body.iconeservum == undefined || req.body.iconeservum == null){
+        errors.push({error: "Necessário preencher o campo ícone do Serviço 1"})
+    }
+    if(!req.body.tituloservum || typeof req.body.tituloservum == undefined || req.body.tituloservum == null){
+        errors.push({error: "Necessário preencher o campo título do Serviço 1"})
+    }
+    if(!req.body.descservum || typeof req.body.descservum == undefined || req.body.descservum == null){
+        errors.push({error: "Necessário preencher o campo descrição do Serviço 1"})
+    }
+    if(!req.body.iconeservdois || typeof req.body.iconeservdois == undefined || req.body.iconeservdois == null){
+        errors.push({error: "Necessário preencher o campo ícone do Serviço 2"})
+    }
+    if(!req.body.tituloservdois || typeof req.body.tituloservdois == undefined || req.body.tituloservdois == null){
+        errors.push({error: "Necessário preencher o campo título do Serviço 2"})
+    }
+    if(!req.body.descservdois || typeof req.body.descservdois == undefined || req.body.descservdois == null){
+        errors.push({error: "Necessário preencher o campo descrição do Serviço 2"})
+    }
+    if(!req.body.iconeservtres || typeof req.body.iconeservtres == undefined || req.body.iconeservtres == null){
+        errors.push({error: "Necessário preencher o campo ícone do Serviço 3"})
+    }
+    if(!req.body.tituloservtres || typeof req.body.tituloservtres == undefined || req.body.tituloservtres == null){
+        errors.push({error: "Necessário preencher o campo título do Serviço 3"})
+    }
+    if(!req.body.descservtres || typeof req.body.descservtres == undefined || req.body.descservtres == null){
+        errors.push({error: "Necessário preencher o campo descrição do Serviço 3"})
+    }
+    
+
+    //Verifica quantos erros tem acima. Caso tenha algum, monta o layout da página novamente
+    //e apresenta os erros que foram armazenados em um array na variável dados_sobre
+
+    //Para testar todos os ifs, retirar o campo 'required' do html 5 no 'edit-sobre.handlebars'
+    if(errors.length > 0){
+        res.render("home/edit-home-serv", { layout: "adm.handlebars", errors: errors, servico: dados_home_serv} )
+    }
+
+    //Caso não tenha erros, pega todos os dados digitados e altera no Banco
+    else{
+        Servico.findOne({ _id: req.body._id }).then((servico) => {
+            servico.titulo = req.body.titulo,
+            servico.iconeservum = req.body.iconeservum,
+            servico.tituloservum = req.body.tituloservum,
+            servico.descservum = req.body.descservum,
+            servico.iconeservdois = req.body.iconeservdois,
+            servico.tituloservdois = req.body.tituloservdois,
+            servico.descservdois = req.body.descservdois,
+            servico.iconeservtres = req.body.iconeservtres,
+            servico.tituloservtres = req.body.tituloservtres,
+            servico.descservtres = req.body.descservtres
+    
+            servico.save().then(() => {
+                req.flash("success_msg", "Editado com sucesso!")
+                res.redirect("/vis-home-serv")
+            }).catch((erro) => {
+                req.flash("error_msg", "Erro: Não foi possível alterar")
+                res.redirect("/dashboard/")
+            })
+        }).catch((erro) => {
+            req.flash("error_msg", "Erro: Nenhum registro encontrado")
+            res.redirect("/dashboard/")
+        })
+    }
+
+    
+})
+
 module.exports = router
